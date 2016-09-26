3752dfab985e4a6a6ccead290eafb26097ea13c0
Sun Kai <Sun Kai@SUNKAI-BJ>
2016-09-26 16:43:30 +0800

    Add project files.

---
+++ FrameNavigation/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace FrameNavigation
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            iFrame.Navigate(typeof(Page1));
+        }
+
+        private void home_Click(object sender, RoutedEventArgs e)
+        {
+            iFrame.Navigate(typeof(Page1));
+        }
+
+        private void back_Click(object sender, RoutedEventArgs e)
+        {
+            if (iFrame.CanGoBack)
+                iFrame.GoBack();
+        }
+
+        private void forward_Click(object sender, RoutedEventArgs e)
+        {
+            if (iFrame.CanGoForward)
+                iFrame.GoForward();
+        }
+
+        private void goto_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Page3));
+        }
+    }
+}
