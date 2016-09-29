253be09e3577a772805a30500397fc90e1234c88
Sun Kai <Sun Kai@SUNKAI-BJ>
2016-09-29 17:33:12 +0800

    Add project files.

---
+++ BindData/Book.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindData
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }       
+
+    }
+    internal class BookManager
+    {
+        internal static List<Book> books = new List<Book>();
+
+        internal static List<Book> GetBooks()
+        {
+            books.Add(new Book { ID = 1000, Title = "Learning C# step by step", Author = "Kevin Sun" });
+            books.Add(new Book { ID = 1000, Title = "Thinking in Java", Author = "Tom Cruise" });
+            books.Add(new Book { ID = 1000, Title = "Introduction to UWP", Author = "Bob Tabor" });
+            books.Add(new Book { ID = 1000, Title = "Create Android Apps", Author = "Google" });
+            books.Add(new Book { ID = 1000, Title = "How to use Visual Studio", Author = "Microsoft" });
+            books.Add(new Book { ID = 1000, Title = "iOS Development", Author = "Apple" });
+
+            return books;
+        }
+    }
+}
